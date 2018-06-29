e9ad50415d2f2995811c0d59b826796d1791416d
slince <taosikai@yeah.net>
2018-06-29 17:44:23 +0800

    init

--- src/SchedulingBundle.php
+++ src/SchedulingBundle.php
@@ -2,8 +2,17 @@
 
 namespace Slince\SchedulingBundle;
 
+use Slince\SchedulingBundle\DependencyInjection\SchedulingExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SchedulingBundle extends Bundle
 {
+
+    /**
+     * @return string
+     */
+    public function getContainerExtensionClass()
+    {
+        return SchedulingExtension::class;
+    }
 }
